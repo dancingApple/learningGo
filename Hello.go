fb722a054671f7122b68f30f4196cb192c8564dd
Michael Han <supersonicmaser@gmail.com>
2019-06-03 15:12:15 +1000

    add commits

--- Hello.go
+++ Hello.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	/* "_" means throw away returns */
 	n, _ := fmt.Println("This is a String", 28, true)
 	fmt.Println("The number of bytes written is",n)
+
+	/* Declare and assign value using short declaration operator */
 	num := 78
 	fmt.Println("Declare variable num and assign its value to",num)
 	num = 56
