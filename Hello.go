ba6c193c92ab1ff05d3ac8aacdb5a09e08eb001f
Michael Han <supersonicmaser@gmail.com>
2019-06-03 14:56:40 +1000

    play with the value declaration and re-assign

--- Hello.go
+++ Hello.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	n, _ := fmt.Println("This is a String", 28, true)
 	fmt.Println("The number of bytes written is",n)
+	num := 78
+	fmt.Println("Declare variable num and assign its value to",num)
+	num = 56
+	fmt.Println("Re-assign num value to",num)
 }
