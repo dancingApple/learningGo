e121fa535b6877b897c91e6793f1e0c52b46dded
Michael Han <supersonicmaser@gmail.com>
2019-06-04 12:51:05 +1000

    explore more with type and string

--- exec1.go
+++ exec1.go
@@ -4,6 +4,12 @@ import "fmt"
 
 var x = 12
 var y int
+var str = "Is that true?"
+var ans = true
+type cat int
+var k cat
+type kitten cat
+var f kitten
 
 func main() {
 	z := 16
@@ -12,4 +18,15 @@ func main() {
 	fmt.Printf("%v\n%b\n", x, x)
 	fmt.Printf("%v\n%X\n", y, y)
 	fmt.Printf("%v\n%T\n", z, z)
+	s := fmt.Sprintf("%v\t%v\t%v\t%v\t%v",x,y,z,str,ans)
+	fmt.Println(s)
+	fmt.Println(k)
+	fmt.Printf("%T\n",k)
+	k = 42
+	fmt.Println(k)
+	f = kitten(k)
+	fmt.Println("The number of kitten is",f)
+	fmt.Printf("%T\n",f)
+	strToByte := []byte(str)
+	fmt.Println(strToByte)
 }
